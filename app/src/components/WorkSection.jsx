2686a6b565e2fb4925fe547c87030e5a081611b9
ashsic <99445200+ashsic@users.noreply.github.com>
2024-02-05 17:56:27 -0500

    feat: can now add and remove job experiences

--- app/src/components/WorkSection.jsx
+++ app/src/components/WorkSection.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 
 function WorkSection() {
-
   const [isDisplayed, setIsDisplayed] = useState(false);
+  const showForm = () => setIsDisplayed(!isDisplayed);
 
-  const showForm = () => setIsDisplayed(!isDisplayed)
-
-  let workExperiences = [
+  const [workExperiences, setWorkExperiences] = useState([
     {
-      id: 1123,
+      id: 1,
       company: "test company",
       title: "Software Engineer",
       startDate: "2014/02/02",
@@ -16,17 +14,41 @@ function WorkSection() {
       description: "Worked as a software engineer for 'test company'. Did lots of testing and worked hard."
     },
     {
-      id: 2123,
+      id: 2,
       company: "test company 2",
       title: "Software Architect",
       startDate: "2018/02/02",
       endDate: "2021/03/04",
       description: "Worked as a software architect for 'test company 2'. Did lots of architecting and worked hard."
     },
-  ];
+  ]);
+
+  const [idCounter, setIdCounter] = useState(3);
+
+  const incrementIdCounter = () => {
+    setIdCounter(idCounter + 1);
+  }
 
   const filterWork = (event) => {
-    workExperiences = workExperiences.filter(job => job.id !== event.target.id)
+    const newExperiences = workExperiences.filter(job => job.id != event.target.id);
+    setWorkExperiences(newExperiences);
+  }
+
+  const addWork = (event) => {
+    event.preventDefault();
+    const parentContainer = event.target.parentElement;
+    let newExperiences = workExperiences;
+    newExperiences.push({
+      id: idCounter,
+      company: parentContainer.querySelector('#companyName').value,
+      title: parentContainer.querySelector('#jobTitle').value,
+      startDate: parentContainer.querySelector('#jobStartDate').value,
+      endDate: parentContainer.querySelector('#jobEndDate').value,
+      description: parentContainer.querySelector('#jobDescription').value
+    });
+    incrementIdCounter();
+    setWorkExperiences(newExperiences);
+    showForm();
   }
 
   return (
@@ -64,7 +86,7 @@ function WorkSection() {
               <input type="text" id="jobDescription" />
             </div>
 
-            <button onClick={(event) => event.preventDefault()}>Save</button>
+            <button onClick={addWork}>Save</button>
           </form>
         )
       }
@@ -78,7 +100,7 @@ function WorkSection() {
                 <p>{job.startDate} - {job.endDate}</p>
               </div>
               <p>{job.description}</p>
-              <button id="job.id" onClick={() => console.log(job.id)}>X</button>
+              <button id={job.id} onClick={filterWork}>X</button>
             </li>
           )
         })}
